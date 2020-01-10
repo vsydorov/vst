7d140ab5004aa3a2be5f2bb393b5488a320364f4
SYDOROV Vladyslav <vladyslav.sydorov@inria.fr>
2020-01-10 18:34:38 +0100

    Chaos - in the middle of adding logging for MP

--- thes/experiments/demo_jan/demo_torch.py
+++ thes/experiments/demo_jan/demo_torch.py
@@ -1,4 +1,7 @@
 import logging
+import logging.handlers
+import threading
+import argparse
 import time
 import yacs
 from tqdm import tqdm
@@ -11,6 +14,7 @@ import os
 import random
 import numpy as np
 import pandas as pd
+import multiprocessing
 
 from collections import OrderedDict
 from pathlib import Path
@@ -352,28 +356,259 @@ SOLVER:
 """
 
 
-def _train_func(d_cfg, cf):
-    # Register "absolute" VOC
-    VOC_ROOT = '/home/vsydorov/projects/datasets/detection'
-    from detectron2.data.datasets.pascal_voc import register_pascal_voc
-    SPLITS = [
-        ("voc_2007_trainval_absolute", "VOC2007/VOCdevkit/VOC2007", "trainval"),
-        ("voc_2007_test_absolute", "VOC2007/VOCdevkit/VOC2007", "test"),
-        ("voc_2012_trainval_absolute", "VOC2012/VOCdevkit/VOC2012", "trainval"),
-    ]
-    for name, dirname, split in SPLITS:
-        year = 2007 if "2007" in name else 2012
-        register_pascal_voc(name, os.path.join(VOC_ROOT, dirname), split, year)
-        MetadataCatalog.get(name).evaluator_type = "pascal_voc"
+import sys
+import functools
+from fvcore.common.file_io import PathManager
+from detectron2.utils.logger import (
+        setup_logger, _ColorfulFormatter, colored, _cached_log_stream)
+from detectron2.utils.collect_env import collect_env_info
+from detectron2.utils.env import seed_all_rng
 
-    trainer = Trainer(d_cfg)
-    trainer.resume_or_load(resume=cf['resume'])
-    if d_cfg.TEST.AUG.ENABLED:
-        trainer.register_hooks(
-            [hooks.EvalHook(
-                0, lambda: trainer.test_with_TTA(d_cfg, trainer.model))]
+
+@functools.lru_cache()
+def setup_logger_d2mine(
+    output=None, distributed_rank=0, *, color=True, name="detectron2",
+    abbrev_name=None
+):
+    """
+    Args:
+        output (str): a file name or a directory to save log. If None, will not save log file.
+            If ends with ".txt" or ".log", assumed to be a file name.
+            Otherwise, logs will be saved to `output/log.txt`.
+        name (str): the root module name of this logger
+        abbrev_name (str): an abbreviation of the module, to avoid long names in logs.
+            Set to "" to not log the root module in logs.
+            By default, will abbreviate "detectron2" to "d2" and leave other
+            modules unchanged.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
+
+    if abbrev_name is None:
+        abbrev_name = "d2" if name == "detectron2" else name
+
+    plain_formatter = logging.Formatter(
+        "[%(asctime)s] %(name)s %(levelname)s: %(message)s", datefmt="%m/%d %H:%M:%S"
+    )
+    # stdout logging: master only
+    if distributed_rank == 0:
+        ch = logging.StreamHandler(stream=sys.stdout)
+        ch.setLevel(logging.DEBUG)
+        if color:
+            formatter = _ColorfulFormatter(
+                colored("[%(asctime)s %(name)s]: ", "green") + "%(message)s",
+                datefmt="%m/%d %H:%M:%S",
+                root_name=name,
+                abbrev_name=str(abbrev_name),
+            )
+        else:
+            formatter = plain_formatter
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+    # file logging: all workers
+    if output is not None:
+        if output.endswith(".txt") or output.endswith(".log"):
+            filename = output
+        else:
+            filename = os.path.join(output, "log.txt")
+        if distributed_rank > 0:
+            filename = filename + ".rank{}".format(distributed_rank)
+        PathManager.mkdirs(os.path.dirname(filename))
+
+        fh = logging.StreamHandler(_cached_log_stream(filename))
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(plain_formatter)
+        logger.addHandler(fh)
+
+    return logger
+
+
+def default_setup_mine(cfg, args):
+    """
+    Perform some basic common setups at the beginning of a job, including:
+
+    1. Set up the detectron2 logger
+    2. Log basic information about environment, cmdline arguments, and config
+    3. Backup the config to the output directory
+
+    Args:
+        cfg (CfgNode): the full config to be used
+        args (argparse.NameSpace): the command line arguments to be logged
+    """
+    output_dir = cfg.OUTPUT_DIR
+    if comm.is_main_process() and output_dir:
+        PathManager.mkdirs(output_dir)
+
+    rank = comm.get_rank()
+    setup_logger_d2mine(output_dir, distributed_rank=rank, name="fvcore")
+    logger = setup_logger_d2mine(output_dir, distributed_rank=rank)
+
+    logger.info("Rank of current process: {}. World size: {}".format(
+        rank, comm.get_world_size()))
+    logger.info("Environment info:\n" + collect_env_info())
+
+    logger.info("Command line arguments: " + str(args))
+    if hasattr(args, "config_file") and args.config_file != "":
+        logger.info(
+            "Contents of args.config_file={}:\n{}".format(
+                args.config_file,
+                PathManager.open(args.config_file, "r").read()
+            )
         )
-    trainer.train()
+
+    logger.info("Running with full config:\n{}".format(cfg))
+    if comm.is_main_process() and output_dir:
+        # Note: some of our scripts may expect the existence of
+        # config.yaml in output directory
+        path = os.path.join(output_dir, "config.yaml")
+        with PathManager.open(path, "w") as f:
+            f.write(cfg.dump())
+        logger.info("Full config saved to {}".format(os.path.abspath(path)))
+
+    # make sure each worker has a different, yet deterministic seed if specified
+    seed_all_rng(None if cfg.SEED < 0 else cfg.SEED + rank)
+
+    # cudnn benchmark has large overhead.
+    # It shouldn't be used considering the small size of
+    # typical validation set.
+    if not (hasattr(args, "eval_only") and args.eval_only):
+        torch.backends.cudnn.benchmark = cfg.CUDNN_BENCHMARK
+
+
+def logger_thread(q):
+    while True:
+        record = q.get()
+        if record is not None:
+            print('Got record {}, name {}:'.format(record, record.name))
+        if record is None:
+            print('Got NONE record')
+            break
+        logger = logging.getLogger(record.name)
+        logger.handle(record)
+
+
+def _train_func(d_cfg, cf, args, logmsg_queue):
+    # default_setup_mine(d_cfg, args)
+    #
+    # d2_log = logging.getLogger("detectron2")
+    # d2_log.info("d2 test")
+    #
+    print('before_handlers')
+    print('__name__: {}'.format(__name__))
+
+    root_log = logging.getLogger()
+    print('ROOT handlers: {}'.format(root_log.handlers))
+
+    q_handler = logging.handlers.QueueHandler(logmsg_queue)
+    q_log = logging.getLogger(__name__)
+    q_log.addHandler(q_handler)
+    q_log.setLevel(logging.DEBUG)
+    # q_log.propagate = False
+    print('q_log handlers: {}'.format(q_log.handlers))
+    #
+    name = multiprocessing.current_process().name
+
+    q_log.info("q_log test, name {}".format(name))
+    log.info("log test, name {}".format(name))
+
+    print('after everything')
+    # log.info("my test")
+
+    #     ("voc_2007_trainval_absolute", "VOC2007/VOCdevkit/VOC2007", "trainval"),
+    #     ("voc_2007_test_absolute", "VOC2007/VOCdevkit/VOC2007", "test"),
+    #     ("voc_2012_trainval_absolute", "VOC2012/VOCdevkit/VOC2012", "trainval"),
+    # ]
+    # for name, dirname, split in SPLITS:
+    #     year = 2007 if "2007" in name else 2012
+    #     register_pascal_voc(name, os.path.join(VOC_ROOT, dirname), split, year)
+    #     MetadataCatalog.get(name).evaluator_type = "pascal_voc"
+    #
+    # trainer = Trainer(d_cfg)
+    # trainer.resume_or_load(resume=cf['resume'])
+    # if d_cfg.TEST.AUG.ENABLED:
+    #     trainer.register_hooks(
+    #         [hooks.EvalHook(
+    #             0, lambda: trainer.test_with_TTA(d_cfg, trainer.model))]
+    #     )
+    # trainer.train()
+
+
+from detectron2.engine.launch import _find_free_port, _distributed_worker
+import torch.multiprocessing as torch_mp
+import torch.distributed as dist
+
+
+def _my_distributed_worker(
+        local_rank, main_func, world_size,
+        num_gpus_per_machine, machine_rank,
+        dist_url, args):
+    assert torch.cuda.is_available(), \
+            "cuda is not available. Please check your installation."
+    global_rank = machine_rank * num_gpus_per_machine + local_rank
+    try:
+        dist.init_process_group(
+            backend="NCCL", init_method=dist_url,
+            world_size=world_size, rank=global_rank
+        )
+    except Exception as e:
+        logger = logging.getLogger(__name__)
+        logger.error("Process group URL: {}".format(dist_url))
+        raise e
+    # synchronize is needed here to prevent a possible timeout after calling
+    # init_process_group
+    # See: https://github.com/facebookresearch/maskrcnn-benchmark/issues/172
+    comm.synchronize()
+
+    assert num_gpus_per_machine <= torch.cuda.device_count()
+    torch.cuda.set_device(local_rank)
+
+    # Setup the local process group (which contains ranks within the same
+    # machine)
+    assert comm._LOCAL_PROCESS_GROUP is None
+    num_machines = world_size // num_gpus_per_machine
+    for i in range(num_machines):
+        ranks_on_i = list(range(i * num_gpus_per_machine,
+            (i + 1) * num_gpus_per_machine))
+        pg = dist.new_group(ranks_on_i)
+        if i == machine_rank:
+            comm._LOCAL_PROCESS_GROUP = pg
+
+    main_func(*args)
+
+
+def my_launch(main_func, num_gpus_per_machine, num_machines=1, machine_rank=0,
+        dist_url=None, args=()):
+    """
+    Args:
+        main_func: a function that will be called by `main_func(*args)`
+        num_machines (int): the total number of machines
+        machine_rank (int): the rank of this machine (one per machine)
+        dist_url (str): url to connect to for distributed training, including protocol
+                       e.g. "tcp://127.0.0.1:8686".
+                       Can be set to auto to automatically select a free port on localhost
+        args (tuple): arguments passed to main_func
+    """
+    world_size = num_machines * num_gpus_per_machine
+    if world_size > 1:
+        # https://github.com/pytorch/pytorch/pull/14391
+        # TODO prctl in spawned processes
+
+        if dist_url == "auto":
+            assert num_machines == 1, "dist_url=auto cannot work with distributed training."
+            port = _find_free_port()
+            dist_url = f"tcp://127.0.0.1:{port}"
+
+        torch_mp.spawn(
+            _distributed_worker,
+            nprocs=num_gpus_per_machine,
+            args=(main_func, world_size, num_gpus_per_machine, machine_rank,
+                dist_url, args),
+            daemon=False,
+        )
+    else:
+        main_func(*args)
 
 
 def train_d2_rcnn_voc0712(workfolder, cfg_dict, add_args):
@@ -413,12 +648,33 @@ def train_d2_rcnn_voc0712(workfolder, cfg_dict, add_args):
     port = 2 ** 15 + 2 ** 14 + hash(os.getuid()) % 2 ** 14
     dist_url = "tcp://127.0.0.1:{}".format(port)
 
-    if cf['num_gpus'] > 1:
-        launch(_train_func,
-                cf['num_gpus'],
-                num_machines=1,
-                machine_rank=0,
-                dist_url=dist_url,
-                args=(d_cfg, cf))
-    else:
-        _train_func(d_cfg, cf)
+    args = argparse.Namespace()
+
+    # if cf['num_gpus'] > 1:
+    #     launch(_train_func,
+    #             cf['num_gpus'],
+    #             num_machines=1,
+    #             machine_rank=0,
+    #             dist_url=dist_url,
+    #             args=(d_cfg, cf, args))
+    # else:
+    #     _train_func(d_cfg, cf, args)
+
+    mp = multiprocessing.get_context('spawn')
+    logmsg_queue = mp.Queue()
+    lp = threading.Thread(target=logger_thread, args=(logmsg_queue,))
+    lp.start()
+    my_launch(_train_func,
+            cf['num_gpus'],
+            num_machines=1,
+            machine_rank=0,
+            dist_url=dist_url,
+            args=(d_cfg, cf, args, logmsg_queue))
+    # _train_func(d_cfg, cf, args, logmsg_queue)
+    logmsg_queue.put(None)
+    lp.join()
+
+    root_log = logging.getLogger()
+    print('ROOT handlers: {}'.format(root_log.handlers))
+
+    # _train_func(d_cfg, cf, args)
