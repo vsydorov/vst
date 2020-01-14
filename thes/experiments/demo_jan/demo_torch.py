df734da47e3e12327ed48d0c0e3e549831b4a807
SYDOROV Vladyslav <vladyslav.sydorov@inria.fr>
2020-01-14 22:22:06 +0100

    DALY training should work

--- thes/experiments/demo_jan/demo_torch.py
+++ thes/experiments/demo_jan/demo_torch.py
@@ -1,4 +1,5 @@
 import cv2
+import copy
 import torch
 import logging
 import logging.handlers
@@ -24,7 +25,6 @@ from typing import Dict, List, Tuple
 from mypy_extensions import TypedDict
 
 from detectron2.config import get_cfg
-from detectron2.data import MetadataCatalog
 from detectron2.engine.defaults import DefaultPredictor
 from detectron2.utils.video_visualizer import VideoVisualizer
 from detectron2.utils.visualizer import ColorMode, Visualizer
@@ -46,11 +46,22 @@ from detectron2.evaluation import (
 import detectron2.utils.comm as comm
 from detectron2.modeling import GeneralizedRCNNWithTTA
 from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.structures import BoxMode
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.data import detection_utils as d2_dutils
+from detectron2.data import transforms as d2_transforms
+from detectron2.data import (
+        build_detection_train_loader,
+        build_detection_test_loader)
+from PIL import Image
+from fvcore.common.file_io import PathManager
 
 from vsydorov_tools import small
+from vsydorov_tools import cv as vt_cv
 
+from thes.data import video_utils
 from thes.tools import snippets
-from thes.data.external_dataset import DatasetVOC2007
+from thes.data.external_dataset import DatasetVOC2007, DatasetDALY
 from thes.det2 import launch_w_logging, simple_d2_setup
 
 
@@ -433,3 +444,375 @@ def train_d2_rcnn_voc0712(workfolder, cfg_dict, add_args):
             machine_rank=0,
             dist_url=dist_url,
             args=(d_cfg, cf, args))
+
+
+def daly_to_datalist(dataset, split):
+    split_vids = [vid for vid, split in dataset.split.items()
+            if split == split]
+
+    d2_datalist = []
+    for vid in tqdm(split_vids):
+        v = dataset.video_odict[vid]
+        vmp4 = dataset.source_videos[vid]
+        video_path = vmp4['video_path']
+        height = vmp4['height']
+        width = vmp4['width']
+        for action_name, instances in v['instances'].items():
+            for ins_ind, instance in enumerate(instances):
+                for keyframe in instance['keyframes']:
+                    # box = keyframe['boundingBox']
+                    fn = keyframe['frameNumber']
+                    image_id = '{}_A{}_FN{}'.format(vid, action_name, fn)
+                    kf_objects = keyframe['objects']
+                    annotations = []
+                    for kfo in kf_objects:
+                        [xmin, ymin, xmax, ymax,
+                            objectID, isOccluded, isHallucinate] = kfo
+                        if isHallucinate:
+                            continue
+                        box_unscaled = np.array([xmin, ymin, xmax, ymax])
+                        bbox = box_unscaled * np.tile([width, height], 2)
+                        bbox_mode = BoxMode.XYXY_ABS
+                        obj = {
+                                'bbox': bbox,
+                                'bbox_mode': bbox_mode,
+                                'category_id': int(objectID)}
+                        annotations.append(obj)
+                    if len(annotations) == 0:
+                        continue
+                    record = {
+                            'video_path': video_path,
+                            'video_frame_number': fn,
+                            'image_id': image_id,
+                            'height': vmp4['height'],
+                            'width': vmp4['width'],
+                            'annotations': annotations}
+                    d2_datalist.append(record)
+    return d2_datalist
+
+
+def demo_d2_dalyobj_vis(workfolder, cfg_dict, add_args):
+    out, = snippets.get_subfolders(workfolder, ['out'])
+    cfg = snippets.YConfig(cfg_dict)
+    cfg.set_deftype("""
+    resume: [True, bool]
+    num_gpus: [~, int]
+    solver_gpuscale: [true, bool]
+    dataset:
+        name: [~, ['daly']]
+        cache_folder: [~, str]
+    """)
+    cf = cfg.parse()
+    # DALY Dataset
+    dataset = DatasetDALY()
+    dataset.populate_from_folder(cf['dataset.cache_folder'])
+
+    datalist_per_split = {}
+    for split in ['train', 'test']:
+        datalist = daly_to_datalist(dataset, split)
+        datalist_per_split[split] = datalist
+
+    for split, datalist in datalist_per_split.items():
+        d2_dataset_name = f'dalyobjects_{split}'
+        DatasetCatalog.register(d2_dataset_name,
+                lambda split=split: datalist_per_split[split])
+        MetadataCatalog.get(d2_dataset_name).set(
+                thing_classes=dataset.object_names)
+
+    # Visualize
+    metadata = MetadataCatalog.get("dalyobjects_train")
+    train_datalist = datalist_per_split['train']
+
+    NP_SEED = 0
+    np_rstate = np.random.RandomState(NP_SEED)
+    prm_train_datalist = np_rstate.permutation(train_datalist)
+    dl_items = prm_train_datalist[:50]
+
+    for i, dl_item in enumerate(dl_items):
+        video_path = dl_item['video_path']
+        frame_number = dl_item['video_frame_number']
+        with vt_cv.video_capture_open(video_path) as vcap:
+            frame_u8 = vt_cv.video_sample(vcap, [frame_number])[0]
+
+        visualizer = Visualizer(frame_u8, metadata=metadata, scale=0.5)
+        img_vis = visualizer.draw_dataset_dict(dl_item)
+        filename = 'i{:02d}_id.jpg'.format(i, dl_item['image_id'])
+        cv2.imwrite(str(out/filename), img_vis.get_image())
+
+
+class DalyVideoDatasetMapper:
+    """
+    A callable which takes a dataset dict in Detectron2 Dataset format,
+    and map it into a format used by the model.
+
+    This is the default callable to be used to map your dataset dict into training data.
+    You may need to follow it to implement your own one for customized logic.
+
+    The callable currently does the following:
+
+    1. Read the image from "file_name"
+    2. Applies cropping/geometric transforms to the image and annotations
+    3. Prepare data and annotations to Tensor and :class:`Instances`
+    """
+
+    def __init__(self, cfg, is_train=True):
+        if cfg.INPUT.CROP.ENABLED and is_train:
+            self.crop_gen = d2_transforms.RandomCrop(
+                    cfg.INPUT.CROP.TYPE, cfg.INPUT.CROP.SIZE)
+            logging.getLogger(__name__).info(
+                    "CropGen used in training: " + str(self.crop_gen))
+        else:
+            self.crop_gen = None
+
+        self.tfm_gens = d2_dutils.build_transform_gen(cfg, is_train)
+
+        # fmt: off
+        self.img_format     = cfg.INPUT.FORMAT
+        self.mask_on        = cfg.MODEL.MASK_ON
+        self.mask_format    = cfg.INPUT.MASK_FORMAT
+        self.keypoint_on    = cfg.MODEL.KEYPOINT_ON
+        self.load_proposals = cfg.MODEL.LOAD_PROPOSALS
+        # fmt: on
+        if self.keypoint_on and is_train:
+            # Flip only makes sense in training
+            self.keypoint_hflip_indices = \
+                    d2_dutils.create_keypoint_hflip_indices(cfg.DATASETS.TRAIN)
+        else:
+            self.keypoint_hflip_indices = None
+
+        if self.load_proposals:
+            self.min_box_side_len = cfg.MODEL.PROPOSAL_GENERATOR.MIN_SIZE
+            self.proposal_topk = (
+                cfg.DATASETS.PRECOMPUTED_PROPOSAL_TOPK_TRAIN
+                if is_train
+                else cfg.DATASETS.PRECOMPUTED_PROPOSAL_TOPK_TEST
+            )
+        self.is_train = is_train
+
+    def __call__(self, dataset_dict):
+        """
+        Args:
+            dataset_dict (dict): Metadata of one image, in Detectron2 Dataset format.
+
+        Returns:
+            dict: a format that builtin models in detectron2 accept
+        """
+        dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+        # # USER: Write your own image loading if it's not from a file
+        # image = d2_dutils.read_image(
+        #         dataset_dict["file_name"], format=self.img_format)
+
+        video_path = dataset_dict['video_path']
+        frame_number = dataset_dict['video_frame_number']
+        with vt_cv.video_capture_open(video_path) as vcap:
+            frame_u8 = vt_cv.video_sample(vcap, [frame_number])[0]
+
+        image = frame_u8
+
+        d2_dutils.check_image_size(dataset_dict, image)
+
+        if "annotations" not in dataset_dict:
+            image, transforms = d2_transforms.apply_transform_gens(
+                ([self.crop_gen] if self.crop_gen else []) + self.tfm_gens, image
+            )
+        else:
+            # Crop around an instance if there are instances in the image.
+            # USER: Remove if you don't use cropping
+            if self.crop_gen:
+                crop_tfm = d2_dutils.gen_crop_transform_with_instance(
+                    self.crop_gen.get_crop_size(image.shape[:2]),
+                    image.shape[:2],
+                    np.random.choice(dataset_dict["annotations"]),
+                )
+                image = crop_tfm.apply_image(image)
+            image, transforms = \
+                    d2_transforms.apply_transform_gens(self.tfm_gens, image)
+            if self.crop_gen:
+                transforms = crop_tfm + transforms
+
+        image_shape = image.shape[:2]  # h, w
+
+        # Pytorch's dataloader is efficient on torch.Tensor due to shared-memory,
+        # but not efficient on large generic data structures due to the use of pickle & mp.Queue.
+        # Therefore it's important to use torch.Tensor.
+        dataset_dict["image"] = torch.as_tensor(
+            image.transpose(2, 0, 1).astype("float32")
+        ).contiguous()
+        # Can use uint8 if it turns out to be slow some day
+
+        # USER: Remove if you don't use pre-computed proposals.
+        if self.load_proposals:
+            d2_dutils.transform_proposals(
+                dataset_dict, image_shape, transforms,
+                self.min_box_side_len, self.proposal_topk
+            )
+
+        if not self.is_train:
+            dataset_dict.pop("annotations", None)
+            dataset_dict.pop("sem_seg_file_name", None)
+            return dataset_dict
+
+        if "annotations" in dataset_dict:
+            # USER: Modify this if you want to keep them for some reason.
+            for anno in dataset_dict["annotations"]:
+                if not self.mask_on:
+                    anno.pop("segmentation", None)
+                if not self.keypoint_on:
+                    anno.pop("keypoints", None)
+
+            # USER: Implement additional transformations if you have other types of data
+            annos = [
+                d2_dutils.transform_instance_annotations(
+                    obj, transforms, image_shape,
+                    keypoint_hflip_indices=self.keypoint_hflip_indices
+                )
+                for obj in dataset_dict.pop("annotations")
+                if obj.get("iscrowd", 0) == 0
+            ]
+            instances = d2_dutils.annotations_to_instances(
+                annos, image_shape, mask_format=self.mask_format
+            )
+            # Create a tight bounding box from masks, useful when image is cropped
+            if self.crop_gen and instances.has("gt_masks"):
+                instances.gt_boxes = instances.gt_masks.get_bounding_boxes()
+            dataset_dict["instances"] = d2_dutils.filter_empty_instances(instances)
+
+        # USER: Remove if you don't do semantic/panoptic segmentation.
+        if "sem_seg_file_name" in dataset_dict:
+            with PathManager.open(dataset_dict.pop("sem_seg_file_name"), "rb") as f:
+                sem_seg_gt = Image.open(f)
+                sem_seg_gt = np.asarray(sem_seg_gt, dtype="uint8")
+            sem_seg_gt = transforms.apply_segmentation(sem_seg_gt)
+            sem_seg_gt = torch.as_tensor(sem_seg_gt.astype("long"))
+            dataset_dict["sem_seg"] = sem_seg_gt
+        return dataset_dict
+
+
+class DalyObjTrainer(DefaultTrainer):
+
+    @classmethod
+    def build_train_loader(cls, cfg):
+        """
+        Returns:
+            iterable
+
+        It now calls :func:`detectron2.data.build_detection_train_loader`.
+        Overwrite it if you'd like a different data loader.
+        """
+        mapper = DalyVideoDatasetMapper(cfg, is_train=True)
+        return build_detection_train_loader(cfg,
+                mapper=mapper)
+
+    @classmethod
+    def build_test_loader(cls, cfg, dataset_name):
+        """
+        Returns:
+            iterable
+
+        It now calls :func:`detectron2.data.build_detection_test_loader`.
+        Overwrite it if you'd like a different data loader.
+        """
+        mapper = DalyVideoDatasetMapper(cfg, is_train=False)
+        return build_detection_test_loader(cfg, dataset_name,
+                mapper=mapper)
+
+    # @classmethod
+    # def build_evaluator(cls, d_cfg, dataset_name, output_folder=None):
+    #     if output_folder is None:
+    #         output_folder = os.path.join(d_cfg.OUTPUT_DIR, "inference")
+    #     return PascalVOCDetectionEvaluator(dataset_name)
+
+
+def _train_func_dalyobj(d_cfg, cf, args):
+    simple_d2_setup(d_cfg)
+
+    trainer = DalyObjTrainer(d_cfg)
+    trainer.resume_or_load(resume=cf['resume'])
+    if d_cfg.TEST.AUG.ENABLED:
+        trainer.register_hooks(
+            [hooks.EvalHook(
+                0, lambda: trainer.test_with_TTA(d_cfg, trainer.model))]
+        )
+    trainer.train()
+
+
+def train_d2_dalyobj(workfolder, cfg_dict, add_args):
+    out, = snippets.get_subfolders(workfolder, ['out'])
+    cfg = snippets.YConfig(cfg_dict)
+    cfg.set_deftype("""
+    resume: [True, bool]
+    num_gpus: [~, int]
+    base_lr: [0.0025, float]
+    eval_period: [0, int]
+    checkpoint_period: [5000, int]
+    solver_gpuscale: [true, bool]
+    dataset:
+        name: [~, ['daly']]
+        cache_folder: [~, str]
+    """)
+    cf = cfg.parse()
+    # DALY Dataset
+    dataset = DatasetDALY()
+    dataset.populate_from_folder(cf['dataset.cache_folder'])
+
+    datalist_per_split = {}
+    for split in ['train', 'test']:
+        datalist = daly_to_datalist(dataset, split)
+        datalist_per_split[split] = datalist
+
+    for split, datalist in datalist_per_split.items():
+        d2_dataset_name = f'dalyobjects_{split}'
+        DatasetCatalog.register(d2_dataset_name,
+                lambda split=split: datalist_per_split[split])
+        MetadataCatalog.get(d2_dataset_name).set(
+                thing_classes=dataset.object_names)
+
+    PRETRAINED_WEIGHTS_MODELPATH = '/home/vsydorov/projects/deployed/2019_12_Thesis/links/horus/pytorch_model_zoo/pascal_voc_baseline/model_final_b1acc2.pkl'
+    CONFIDENCE_THRESHOLD = 0.25
+    # d2_config
+    d_cfg = get_cfg()
+    # Base keys
+    loaded_cfg = yacs.config.CfgNode.load_cfg(YAML_Base_RCNN_C4)
+    d_cfg.merge_from_other_cfg(loaded_cfg)
+    # FRCCN keys
+    loaded_cfg = yacs.config.CfgNode.load_cfg(YAML_faster_rcnn_R_50_C4)
+    d_cfg.merge_from_other_cfg(loaded_cfg)
+    # Manual overwrite
+    d_cfg.MODEL.RETINANET.SCORE_THRESH_TEST = CONFIDENCE_THRESHOLD
+    d_cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = CONFIDENCE_THRESHOLD
+    d_cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = \
+            CONFIDENCE_THRESHOLD
+    d_cfg.OUTPUT_DIR = str(small.mkdir(out/'d2_output'))
+    d_cfg.MODEL.WEIGHTS = PRETRAINED_WEIGHTS_MODELPATH
+    d_cfg.DATASETS.TRAIN = (
+            'dalyobjects_train',)
+    d_cfg.DATASETS.TEST = ('dalyobjects_test',)
+    # Different number of classes
+    d_cfg.MODEL.ROI_HEADS.NUM_CLASSES = 43
+    # No workers
+    # d_cfg.DATALOADER.NUM_WORKERS = 0
+    # GPUs
+    d_cfg.SOLVER.BASE_LR = cf['base_lr'] * cf['num_gpus']
+    d_cfg.SOLVER.IMS_PER_BATCH = 2 * cf['num_gpus']
+    imult = 8 / cf['num_gpus']
+    d_cfg.SOLVER.STEPS = \
+        (np.r_[12000, 16000] * imult).astype(int).tolist()
+    d_cfg.SOLVER.MAX_ITER = \
+        int(18000 * imult)
+    d_cfg.WARMUP_ITERS = int(100 * imult)
+    d_cfg.TEST.EVAL_PERIOD = cf['eval_period']
+    d_cfg.SOLVER.CHECKPOINT_PERIOD = cf['checkpoint_period']
+    d_cfg.freeze()
+
+    port = 2 ** 15 + 2 ** 14 + hash(os.getuid()) % 2 ** 14
+    dist_url = "tcp://127.0.0.1:{}".format(port)
+    args = argparse.Namespace()
+
+    launch_w_logging(_train_func_dalyobj,
+            cf['num_gpus'],
+            num_machines=1,
+            machine_rank=0,
+            dist_url=dist_url,
+            args=(d_cfg, cf, args))
+
+    # _train_func_dalyobj(d_cfg, cf, args)
