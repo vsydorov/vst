Fix MLP0 softmax bug, allow matching freeze5
- Freeze5 and MLP0 will give same results

thes/experiments/m06/finetune.py
@@ -205,6 +205,11 @@ class SlowFast_roitune(M_slowfast):
         init_helper.init_weights(
             self, sf_cfg.MODEL.FC_INIT_STD, sf_cfg.RESNET.ZERO_INIT_FINAL_BN
         )
+        # Weight init
+        last_layer_generator = torch.Generator(2147483647)
+        self.rt_projection.weight.data.normal_(
+            mean=0.0, std=0.01, generator=last_layer_generator)
+        self.rt_projection.weight.data.zero_()
 
     def _construct_roitune(self, sf_cfg, num_classes):
         # DETECTION.ROI_XFORM_RESOLUTION
@@ -354,18 +359,47 @@ def _build_model_addons():
             False, slowfast_alpha, 256)
 
 
-class Head_roitune(nn.Module):
-    def __init__(self, sf_cfg, num_classes):
-        super(Head_roitune, self).__init__()
-        self._construct_roitune(sf_cfg, num_classes)
+class Freezer(object):
+    def __init__(self, model, freeze_level, bn_freeze):
+        self.model = model
+        self.freeze_level = freeze_level
+        self.bn_freeze = bn_freeze
 
-    def _construct_roitune(self, sf_cfg, num_classes):
+    @staticmethod
+    def set_bn_eval(module):
+        if isinstance(module, torch.nn.modules.batchnorm._BatchNorm):
+            module.eval()
+
+    def maybe_freeze_batchnorm(self):
+        if not self.bn_freeze:
+            return
+        for i_child, child in enumerate(self.model.children()):
+            if i_child <= self.freeze_level:
+                child.apply(self.set_bn_eval)
+
+    def set_finetune_level(self):
+        for param in self.model.parameters():
+            param.requires_grad = True
+        # freeze layers until freeze_level (inclusive)
+        for i_child, child in enumerate(self.model.children()):
+            if i_child <= self.freeze_level:
+                for param in child.parameters():
+                    param.requires_grad = False
+        self.maybe_freeze_batchnorm()
+
+
+class Head_roitune(nn.Module):
+    def __init__(self, sf_cfg, num_classes, dropout_rate, debug_outputs):
+        super(Head_roitune, self).__init__()
+        self._construct_roitune(sf_cfg, num_classes, dropout_rate)
+        self.debug_outputs = debug_outputs
+
+    def _construct_roitune(self, sf_cfg, num_classes, dropout_rate):
         # params
         xform_resolution = 7
         resolution = [[xform_resolution] * 2]
         scale_factor = [32]
         dim_in = [sf_cfg.RESNET.WIDTH_PER_GROUP * 32]
-        dropout_rate = 0.5
 
         pi = 0
         roi_align = ROIAlign(
@@ -392,29 +426,39 @@ class Head_roitune(nn.Module):
 
         # B C H W
         x = out.view(out.shape[0], -1)
-        roipooled = x
+        result = {}
+        if self.debug_outputs:
+            result['roipooled'] = x
 
         # Perform dropout.
         if hasattr(self, "rt_dropout"):
             x = self.rt_dropout(x)
+        if self.debug_outputs:
+            result['x_after_dropout'] = x
 
         x = x.view(x.shape[0], -1)
+
+        if self.debug_outputs:
+            result['x_after_view'] = x
+
         x = self.rt_projection(x)
+
+        if self.debug_outputs:
+            result['x_after_proj'] = x
+
         x = self.rt_act(x)
-        return (x, roipooled)
+        result['x_final'] = x
+        return result
 
 
 class C2D_1x1_roitune(M_resnet):
-    def __init__(self, sf_cfg, num_classes):
+    def __init__(self, sf_cfg, num_classes, dropout_rate, debug_outputs):
         super(M_resnet, self).__init__()
         self.norm_module = get_norm(sf_cfg)
         self.enable_detection = sf_cfg.DETECTION.ENABLE
         self.num_pathways = 1
         self._construct_network(sf_cfg)
-        self.head = Head_roitune(sf_cfg, num_classes)
-        init_helper.init_weights(
-            self, sf_cfg.MODEL.FC_INIT_STD,
-            sf_cfg.RESNET.ZERO_INIT_FINAL_BN)
+        self.head = Head_roitune(sf_cfg, num_classes, dropout_rate, debug_outputs)
 
     def forward(self, x, bboxes0):
         # hforward_resnet_nopool
@@ -426,6 +470,18 @@ class C2D_1x1_roitune(M_resnet):
         x = self.s5(x)
         x = self.head(x, bboxes0)
         return x
+
+    def init_weights(self, init_std, seed=42):
+        # Init all weights
+        init_helper.init_weights(
+            self, init_std, False)
+
+        # Specifically init last layer
+        ll_generator = torch.Generator()
+        ll_generator = ll_generator.manual_seed(67280421310679+seed)
+        self.head.rt_projection.weight.data.normal_(
+                mean=0.0, std=init_std, generator=ll_generator)
+        self.head.rt_projection.bias.data.zero_()
 
 
 class TD_over_krgb(torch.utils.data.Dataset):
@@ -450,7 +506,7 @@ def _evaluation_step(model, eval_loader, norm_mean_t, norm_std_t):
     model.eval()
     all_softmaxes_ = []
     for i_batch, (data_input) in enumerate(eval_loader):
-        rgbs, bboxes_t, labels = data_input
+        rgbs, bboxes_t, labels, keyframes = data_input
         frame_list_f32c = [to_gpu_normalize_permute(
                 rgbs, norm_mean_t, norm_std_t)]
         bboxes0_t = add_roi_batch_indices(bboxes_t)
@@ -458,7 +514,8 @@ def _evaluation_step(model, eval_loader, norm_mean_t, norm_std_t):
 
         # pred
         with torch.no_grad():
-            pred = model(frame_list_f32c, bboxes0_c)
+            result = model(frame_list_f32c, bboxes0_c)
+            pred = result['x_final']
         pred_np = pred.cpu().numpy()
         all_softmaxes_.append(pred_np)
     all_softmaxes = np.vstack(all_softmaxes_)
@@ -566,22 +623,6 @@ def _evalline(
     )))
     log.info('Eval_timers: {}'.format(eval_timers.time_str))
 
-def _build_model(n_outputs):
-    # Build model
-    rel_yml_path = 'Kinetics/C2D_8x8_R50_IN1K.yaml'
-    sf_cfg = basic_sf_cfg(rel_yml_path)
-    sf_cfg.NUM_GPUS = 1
-    sf_cfg.DATA.NUM_FRAMES = 1
-    sf_cfg.DATA.SAMPLING_RATE = 1
-
-    # / slowfast.models.build_model
-    model = C2D_1x1_roitune(sf_cfg, n_outputs)
-    # Determine the GPU used by the current process
-    cur_device = torch.cuda.current_device()
-    # Transfer the model to the current GPU device
-    model = model.cuda(device=cur_device)
-    return sf_cfg, model
-
 
 class Manager_checkpoint_name(object):
     ckpt_re = r'model_at_epoch_(?P<i_epoch>\d*).pth.tar'
@@ -660,20 +701,30 @@ class Manager_model_checkpoints(object):
         return start_epoch
 
 
-def set_finetune_level(model, freeze_level=-1):
-    for param in model.parameters():
-        param.requires_grad = True
-    # freeze layers until freeze_level (inclusive)
-    for i_child, child in enumerate(model.children()):
-        if i_child <= freeze_level:
-            for param in child.parameters():
-                param.requires_grad = False
+# def set_finetune_level(model, freeze_level=-1):
+#     for param in model.parameters():
+#         param.requires_grad = True
+#     # freeze layers until freeze_level (inclusive)
+#     for i_child, child in enumerate(model.children()):
+#         if i_child <= freeze_level:
+#             for param in child.parameters():
+#                 param.requires_grad = False
 
 class Manager_model_trainer(object):
     def __init__(self, cf):
+        # Build model
+        rel_yml_path = 'Kinetics/C2D_8x8_R50_IN1K.yaml'
+        sf_cfg = basic_sf_cfg(rel_yml_path)
+        sf_cfg.NUM_GPUS = 1
+        sf_cfg.DATA.NUM_FRAMES = 1
+        sf_cfg.DATA.SAMPLING_RATE = 1
+
+        # / slowfast.models.build_model
+        model = C2D_1x1_roitune(sf_cfg, 10, cf['ll_dropout'], cf['debug_outputs'])
         # prepare model
-        sf_cfg, model = _build_model(10)
-        set_finetune_level(model, cf['freeze_level'])
+        freezer = Freezer(model, cf['freeze.level'],
+                cf['freeze.freeze_bn'])
+        freezer.set_finetune_level()
 
         norm_mean_t = np_to_gpu(sf_cfg.DATA.MEAN)
         norm_std_t = np_to_gpu(sf_cfg.DATA.STD)
@@ -692,11 +743,26 @@ class Manager_model_trainer(object):
 
         self.sf_cfg = sf_cfg
         self.model = model
+        self.freezer = freezer
+        self.freezer = freezer
         self.optimizer = optimizer
         self.norm_mean_t = norm_mean_t
         self.norm_std_t = norm_std_t
         self.loss_fn = loss_fn
         self.man_ckpt = man_ckpt
+
+    def model_to_gpu(self):
+        # Determine the GPU used by the current process
+        cur_device = torch.cuda.current_device()
+        # Transfer the model to the current GPU device
+        self.model = self.model.cuda(device=cur_device)
+
+    def set_train(self):
+        self.model.train()
+        self.freezer.maybe_freeze_batchnorm()
+
+    def set_eval(self):
+        self.model.eval()
 
     def batch_forward(self, data_input):
         rgbs, bboxes_t, labels, keyframes = data_input
@@ -706,7 +772,8 @@ class Manager_model_trainer(object):
         bboxes0_c = bboxes0_t.type(torch.cuda.FloatTensor)
         labels_c = labels.cuda()
 
-        pred_train, roipooled = self.model(frame_list_f32c, bboxes0_c)
+        result = self.model(frame_list_f32c, bboxes0_c)
+        pred_train = result['x_final']
         loss = self.loss_fn(pred_train, labels_c)
         self.optimizer.zero_grad()
         loss.backward()
@@ -776,6 +843,7 @@ def tubefeats_train_model(workfolder, cfg_dict, add_args):
 
     # Training
     torch.manual_seed(initial_seed)
+    torch.cuda.manual_seed(initial_seed)
     rgen = np.random.default_rng(initial_seed)
 
     loss_fn = torch.nn.CrossEntropyLoss(reduction='mean')
@@ -848,7 +916,11 @@ def finetune_preextracted_krgb(workfolder, cfg_dict, add_args):
     split_assignment: ['train/val', ['train/val', 'trainval/test']]
     """)
     cfg.set_defaults("""
-    freeze_level: -1
+    freeze:
+        level: -1
+        freeze_bn: False
+    debug_outputs: False
+    ll_dropout: 0.5
     train:
         lr: 1.0e-5
         weight_decay: 5.0e-2
@@ -857,7 +929,7 @@ def finetune_preextracted_krgb(workfolder, cfg_dict, add_args):
         eval: ~
     period:
         ibatch:
-            loss_log: '0::50'
+            loss_log: '0::10'
     """)
     cf = cfg.parse()
     initial_seed = cf['seed']
@@ -875,7 +947,10 @@ def finetune_preextracted_krgb(workfolder, cfg_dict, add_args):
         _prepare_krgb_datas(cf, dataset, vgroup, sset_train, sset_eval)
 
     torch.manual_seed(initial_seed)
+    torch.cuda.manual_seed(initial_seed)
     man_mtrainer = Manager_model_trainer(cf)
+    man_mtrainer.model.init_weights(0.01, seed=initial_seed)
+    man_mtrainer.model_to_gpu()
 
     n_epochs = 40
     period_ibatch_loss_log = cf['period.ibatch.loss_log']
@@ -890,7 +965,7 @@ def finetune_preextracted_krgb(workfolder, cfg_dict, add_args):
     start_epoch = (man_mtrainer.man_ckpt
             .restore_model_magic(checkpoint_path))
 
-    man_mtrainer.model.train()
+    man_mtrainer.set_train()
     for i_epoch in range(start_epoch, n_epochs):
         for i_batch, (data_input) in enumerate(train_krgb_loader):
             loss = man_mtrainer.batch_forward(data_input)
@@ -899,7 +974,8 @@ def finetune_preextracted_krgb(workfolder, cfg_dict, add_args):
                 Nb = len(train_krgb_loader)
                 log.info(f'{i_epoch=}, {i_batch=}/{Nb}: loss {loss.item()}')
         man_mtrainer.man_ckpt.save_epoch(rundir, i_epoch)
-        man_mtrainer.model.eval()
+        man_mtrainer.set_eval()
+        import pudb; pudb.set_trace()  # XXX BREAKPOINT
         man_mtrainer.evaluate_print(eval_krgb_loader, keyframes_sseval,
                 dataset, tubes_dgt_eval, tubes_dwein_eval)
-        man_mtrainer.model.train()
+        man_mtrainer.set_train()

thes/experiments/m06/mlp.py
@@ -15,6 +15,8 @@ import torch.nn.functional as F
 
 from vsydorov_tools import small
 
+import slowfast.utils.weight_init_helper as init_helper
+
 from thes.data.dataset.daly import (
     Ncfg_daly, load_gt_and_wein_tubes)
 from thes.data.dataset.external import (
@@ -99,36 +101,71 @@ class Ncfg_kfeats:
 
 
 class Net_mlp_zerolayer(nn.Module):
-    def __init__(self, D_in, D_out, dropout_rate=0.5):
+    """
+    This one will match outputs with the last layer finetuning in
+    SoftMax-derived convnent
+    """
+    def __init__(
+            self, D_in, D_out,
+            dropout_rate, debug_outputs=False):
         super().__init__()
         if dropout_rate > 0.0:
             self.dropout = nn.Dropout(dropout_rate)
-        self.linear1 = nn.Linear(D_in, D_out)
+        self.rt_projection = nn.Linear(D_in, D_out, bias=True)
         self.rt_act = nn.Softmax(dim=-1)
 
-    def forward(self, x):
+    def init_weights(self, init_std, seed):
+        """Will match initialization of ll finetuned net"""
+        ll_generator = torch.Generator()
+        ll_generator = ll_generator.manual_seed(67280421310679+seed)
+
+        self.rt_projection.weight.data.normal_(
+                mean=0.0, std=init_std, generator=ll_generator)
+        self.rt_projection.bias.data.zero_()
+
+    def forward(self, x, debug_outputs=False):
+        result = {}
         if hasattr(self, "dropout"):
             x = self.dropout(x)
-        x = self.linear1(x)
-        x = self.rt_act(x)
-        return x
+        if debug_outputs:
+            result['x_after_dropout'] = x
+        x = self.rt_projection(x)
+        if debug_outputs:
+            result['x_after_proj'] = x
+        if not self.training:
+            x = self.rt_act(x)
+        result['x_final'] = x
+        return result
 
 
 class Net_mlp_onelayer(nn.Module):
-    def __init__(self, D_in, D_out, H, dropout_rate=0.5):
+    def __init__(self, D_in, D_out, H, dropout_rate):
         super().__init__()
         self.linear1 = nn.Linear(D_in, H)
         self.linear2 = nn.Linear(H, D_out)
-        self.dropout = nn.Dropout(dropout_rate)
-        # self.bn = nn.BatchNorm1d(H, momentum=0.1)
+        if dropout_rate > 0.0:
+            self.dropout = nn.Dropout(dropout_rate)
+        self.rt_act = nn.Softmax(dim=-1)
+
+    def init_weights(self, init_std, seed):
+        ll_generator = torch.Generator()
+        ll_generator = ll_generator.manual_seed(67280421310679+seed)
+        for l in [self.linear1, self.linear2]:
+            l.weight.data.normal_(
+                    mean=0.0, std=init_std, generator=ll_generator)
+            l.bias.data.zero_()
 
     def forward(self, x):
         x = self.linear1(x)
         x = F.relu(x)
-        x = self.dropout(x)
-        # x = self.bn(x)
+        if hasattr(self, "dropout"):
+            x = self.dropout(x)
         x = self.linear2(x)
-        return x
+        if not self.training:
+            x = self.rt_act(x)
+        result = {}
+        result['x_final'] = x
+        return result
 
 
 def fitscale_kfeats(kfeats_d):
@@ -229,7 +266,7 @@ def _quick_kf_eval(kfeat_t, model, cutoff_last_dim):
     def _qacc(pred, Y):
         return pred.argmax(1).eq(Y).sum().item()/len(Y)
     with torch.no_grad():
-        pred_eval = model(kfeat_t['X'])
+        pred_eval = model(kfeat_t['X'])['x_final']
     if cutoff_last_dim:
         pred_eval = pred_eval[:, :-1]
     acc = _qacc(pred_eval, kfeat_t['Y'])
@@ -262,11 +299,11 @@ class Data_access_big(object):
 
 def _eval_tube_softmaxes(model, da_big, dwtis):
     tube_sofmaxes = {}
+    model.eval()
     with torch.no_grad():
         for dwti in dwtis:
-            preds = model(da_big.get(model, dwti))
-            preds = softmax(preds, axis=-1)
-            tube_sofmaxes[dwti] = preds.numpy()
+            preds_softmax = model(da_big.get(model, dwti))['x_final']
+            tube_sofmaxes[dwti] = preds_softmax.numpy()
     return tube_sofmaxes
 
 
@@ -325,10 +362,11 @@ def _kffeats_eval(
     # GT-overlap
     kacc_train = _quick_kf_eval(tkfeats_train, model, False)
     kacc_eval = _quick_kf_eval(tkfeats_eval, model, False)
+    model.eval()
     with torch.no_grad():
-        Y_test = model(tkfeats_eval['X']).numpy()
-    Y_test_pred = np.argmax(Y_test, axis=1)
-    Y_test_softmax = softmax(Y_test, axis=1).copy()
+        # Already softmax when in eval mode
+        Y_test_softmax = model(tkfeats_eval['X'])['x_final'].numpy()
+    Y_test_pred = np.argmax(Y_test_softmax, axis=1)
     kf_acc = accuracy_score(tkfeats_eval['Y'], Y_test_pred)
 
     kf_roc_auc = roc_auc_score(tkfeats_eval['Y'],
@@ -404,6 +442,19 @@ def _kffeats_display_evalresults(result, sset_eval):
         log.info('Full tube AP357: {}'.format(apline))
 
 
+class Full_train_sampler(object):
+    def __init__(self, tkfeats_train):
+        self.tkfeats_train = tkfeats_train
+
+    def train_step(self, model, optimizer, loss_fn, i_epoch):
+        pred_train = model(self.tkfeats_train['X'])['x_final']
+        loss = loss_fn(pred_train, self.tkfeats_train['Y'])
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        return loss
+
+
 def _kffeats_experiment(
         cf, initial_seed, da_big, tkfeats_train,
         tkfeats_eval, tubes_dwein_eval, tubes_dgt_eval,
@@ -415,44 +466,46 @@ def _kffeats_experiment(
 
     n_epochs = cf['train.n_epochs']
     D_in = tkfeats_train['X'].shape[-1]
-    if cf['net.kind'] == 'layer0':
-        model = Net_mlp_zerolayer(D_in, 10)
-    elif cf['net.kind'] == 'layer1':
-        model = Net_mlp_onelayer(D_in, 10, cf['net.layer1.H'])
-    else:
-        raise NotImplementedError()
+    model = define_mlp_model(cf, D_in, 10)
+    model.init_weights(0.01, initial_seed)
+
     loss_fn = torch.nn.CrossEntropyLoss(reduction='mean')
     optimizer = torch.optim.AdamW(model.parameters(),
             lr=cf['train.lr'], weight_decay=cf['train.weight_decay'])
 
-    for epoch in range(n_epochs):
-        out_train = model(tkfeats_train['X'])
-        loss = loss_fn(out_train, tkfeats_train['Y'])
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        if snippets.check_step_sslice(epoch, period_log):
+    if cf['train.mode'] == 'full':
+        trsampler = Full_train_sampler(tkfeats_train)
+    elif cf['train.mode'] == 'partial':
+        train_batch_size = cf['train.partial.train_batch_size']
+        trsampler = Partial_Train_sampler(
+                tkfeats_train, initial_seed, train_batch_size)
+    else:
+        raise NotImplementedError()
+
+    for i_epoch in range(n_epochs):
+        loss = trsampler.train_step(model, optimizer, loss_fn, i_epoch)
+        if snippets.check_step_sslice(i_epoch, period_log):
             model.eval()
             kacc_train = _quick_kf_eval(tkfeats_train, model, False)
             kacc_eval = _quick_kf_eval(tkfeats_eval, model, False)
             model.train()
-            log.info(f'{epoch}: {loss.item():.4f} '
+            log.info(f'{i_epoch}: {loss.item():.4f} '
                     f'K.Acc.Train: {kacc_train*100:.2f}; '
                     f'K.Acc.Eval: {kacc_eval*100:.2f}')
-        if snippets.check_step_sslice(epoch, period_eval_evalset):
+        if snippets.check_step_sslice(i_epoch, period_eval_evalset):
             model.eval()
             result = _kffeats_eval(
                     cf, model, da_big, tkfeats_train, tkfeats_eval,
                     tubes_dwein_eval, tubes_dgt_eval, dataset)
             model.train()
-            log.info(f'Perf at {epoch=}')
+            log.info(f'Perf at {i_epoch=}')
             _kffeats_display_evalresults(result, sset_eval)
     # / Final evaluation
     model.eval()
     result = _kffeats_eval(
             cf, model, da_big, tkfeats_train, tkfeats_eval,
             tubes_dwein_eval, tubes_dgt_eval, dataset)
-    log.info(f'Perf at {epoch=}')
+    log.info(f'Perf at {i_epoch=}')
     _kffeats_display_evalresults(result, sset_eval)
     return result
 
@@ -473,6 +526,44 @@ class TD_over_feats(torch.utils.data.Dataset):
         keyframe['do_not_collate'] = True
         return feat, label, keyframe
 
+def define_mlp_model(cf, D_in, D_out):
+    dropout_rate = cf['net.ll_dropout']
+    if cf['net.kind'] == 'layer0':
+        model = Net_mlp_zerolayer(
+            D_in, D_out, dropout_rate)
+    elif cf['net.kind'] == 'layer1':
+        model = Net_mlp_onelayer(
+            D_in, D_out, cf['net.layer1.H'], dropout_rate)
+    return model
+
+class Partial_Train_sampler(object):
+    def __init__(self, tkfeats_train, initial_seed, TRAIN_BATCH_SIZE):
+        td_sstrain = TD_over_feats(tkfeats_train['X'], tkfeats_train['Y'], tkfeats_train['kf'])
+        train_sampler_rgen = np.random.default_rng(initial_seed)
+        sampler = NumpyRandomSampler(td_sstrain, train_sampler_rgen)
+
+        self.train_krgb_loader = torch.utils.data.DataLoader(td_sstrain,
+                batch_size=TRAIN_BATCH_SIZE,
+                num_workers=0, pin_memory=True,
+                sampler=sampler, shuffle=False,
+                collate_fn=sequence_batch_collate_v2)
+        self.period_ibatch_loss_log = '0::10'
+
+    def train_step(self, model, optimizer, loss_fn, i_epoch):
+        for i_batch, (data_input) in enumerate(self.train_krgb_loader):
+            feats, labels, keyframes = data_input
+            result = model(feats)
+            pred_train = result['x_final']
+            loss = loss_fn(pred_train, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if snippets.check_step_sslice(
+                    i_batch, self.period_ibatch_loss_log):
+                Nb = len(self.train_krgb_loader)
+                log.info(f'{i_epoch=}, {i_batch=}/{Nb}: loss {loss.item()}')
+        return loss
+
 
 def _tubefeats_pretraining(cf, model, loss_fn,
         tkfeats_train, tkfeats_eval):
@@ -484,7 +575,7 @@ def _tubefeats_pretraining(cf, model, loss_fn,
     pretrain_period_log = cf['kf_pretrain.period.log']
     model.train()
     for epoch in range(pretrain_n_epochs):
-        out_train = model(tkfeats_train['X'])
+        out_train = model(tkfeats_train['X'])['x_final']
         loss = loss_fn(out_train, tkfeats_train['Y'])
         pretrain_optimizer.zero_grad()
         loss.backward()
@@ -542,7 +633,7 @@ def _tubefeats_training(cf, model, loss_fn, rgen, da_big,
         loader = da_big.produce_loader(rgen, dwti_to_label_train,
             tubes_per_batch, frames_per_tube)
         for i_batch, (feats, labels) in enumerate(loader):
-            pred_train = model(feats)
+            pred_train = model(feats)['x_final']
             loss = loss_fn(pred_train, labels)
             optimizer.zero_grad()
             loss.backward()
@@ -581,12 +672,7 @@ def _tubefeats_experiment(
     rgen = np.random.default_rng(initial_seed)
 
     D_in = da_big.BIG.shape[-1]
-    if cf['net.kind'] == 'layer0':
-        model = Net_mlp_zerolayer(D_in, 11)
-    elif cf['net.kind'] == 'layer1':
-        model = Net_mlp_onelayer(D_in, 11, cf['net.layer1.H'])
-    else:
-        raise NotImplementedError()
+    model = define_mlp_model(cf, D_in, 11)
     loss_fn = torch.nn.CrossEntropyLoss(reduction='mean')
     # pretraining
     if cf['kf_pretrain.enabled']:
@@ -636,6 +722,11 @@ def kffeats_train_mlp(workfolder, cfg_dict, add_args):
         kind: ['layer1', ['layer0', 'layer1']]
         layer1:
             H: [32, int]
+        ll_dropout: [0.5, float]
+    train:
+        mode: ['full', ['full', 'partial']]
+        partial:
+            train_batch_size: [32, int]
     """)
     cfg.set_defaults("""
     train:
@@ -647,7 +738,7 @@ def kffeats_train_mlp(workfolder, cfg_dict, add_args):
             eval: '0::500'
     eval:
         full_tubes: False
-    n_trials: 5
+    n_trials: 1
     """)
     cf = cfg.parse()
     # params
@@ -734,6 +825,7 @@ def tubefeats_train_mlp(workfolder, cfg_dict, add_args):
         kind: ['layer1', ['layer0', 'layer1']]
         layer1:
             H: [32, int]
+        ll_dropout: [0.5, float]
     """)
     cfg.set_defaults("""
     train: