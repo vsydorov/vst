92278bacd4daf1398cd3e85b2e0a50810f6a5170
Vladyslav Sydorov <vsydorov@yandex.ru>
2020-11-04 16:29:10 +0100

    Clean up this vst

--- vst/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-VST - Vladyslav Sydorov Tools
-
-Lib contains useful snippets and other boilerplate code
-"""
-from .small import *
-from . import (exp, isave, plot, small)
-__all__ = ['exp', 'isave', 'plot', 'small']
