b7ee2d0c77df25f54d5507217220083543d87e17
SYDOROV Vladyslav <vladyslav.sydorov@inria.fr>
2020-12-06 19:05:09 +0100

    Add vst again

---
+++ thes/experiments/m12/feature_extraction.py
@@ -0,0 +1,6 @@
+def extract_keyframe_features(workfolder, cfg_dict, add_args):
+    out, = snippets.get_subfolders(workfolder, ['out'])
+    cfg = snippets.YConfig(cfg_dict)
+    Ncfg_daly.set_defcfg(cfg)
+    Ncfg_extractor.set_defcfg(cfg)
+    cf = cfg.parse()
